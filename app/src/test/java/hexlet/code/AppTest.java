4748bac7dc94fa4b0ed542b4a926ba96d39bb6bd
Diana Lomakina <diana_st16@mail.ru>
2025-03-14 13:51:32 +0300

    changed

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -23,7 +23,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class AppTest {
+public final class AppTest {
     public static Javalin app;
     public static MockWebServer mockServer;
     @BeforeAll
